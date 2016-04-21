02ad57cae0b8164c790673565798d44744a56bd5
Anil Maharjan <anil.maharjan@itonics.de>
2016-04-21 18:45:08 +0200

    hey new port env

--- server.js
+++ server.js
@@ -68,5 +68,6 @@ app.use(function (err, req, res, next) {
 io.sockets.on('connection', function (socket){twing.socketOnConnectionCallback(io,socket)});
 
 
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002, process.env.OPENSHIFT_NODEJS_IP || "127.9.201.1");
 
 module.exports = app;
